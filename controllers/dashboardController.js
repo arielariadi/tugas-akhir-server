da4374e42c28f2ced3416de62e91644954e41209
Muhammad Ariel Ariadi <ariel.ariadi123@gmail.com>
2024-03-29 14:45:56 +0700

    Penyesuaian kode back end

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,7 +2,6 @@ const {
 	GolDarah,
 	LokasiPmi,
 	TraDonor,
-	TraReqDarah,
 	User,
 	sequelize,
 	Sequelize,
@@ -33,48 +32,12 @@ exports.getDashboardUser = async (req, res) => {
 			],
 		});
 
-		// Retrieve blood donation requests made by the user
-		// const userRequesterData = await TraReqDarah.findAll({
-		// 	where: { id_user_volunteer: userId, status: 1 }, // Ensure status corresponds to unconfirmed requests
-		// 	include: [
-		// 		{ model: GolDarah, attributes: ['id_gol_darah', 'gol_darah'] },
-		// 		{ model: User, attributes: ['id_user', 'nama', 'email'] },
-		// 	],
-		// });
-
-		// const userAcceptData = await TraReqDarah.findAll({
-		// 	where: { id_user_volunteer: userId, status: 2 },
-		// 	include: [
-		// 		{ model: GolDarah, attributes: ['id_gol_darah', 'gol_darah'] },
-		// 		{
-		// 			model: User,
-		// 			attributes: ['id_user', 'nama', 'email', 'alamat', 'no_hp'],
-		// 		},
-		// 	],
-		// });
-
-		// const userRejectData = await TraReqDarah.findAll({
-		// 	where: { id_user_volunteer: userId, status: 0 },
-		// 	include: [
-		// 		{ model: GolDarah, attributes: ['id_gol_darah', 'gol_darah'] },
-		// 		{
-		// 			model: User,
-		// 			attributes: ['id_user', 'nama', 'email', 'alamat', 'no_hp'],
-		// 		},
-		// 	],
-		// });
-
 		const pendonors = traDonors.map(traDonor => ({
 			id_user: traDonor.id_user,
 			id_donor: traDonor.id_tra_donor,
 			id_gol_darah: traDonor.id_gol_darah,
 			gol_darah: traDonor.GolDarah.gol_darah,
 			lokasi_pmi: traDonor.LokasiPmi ? traDonor.LokasiPmi.nama : null,
-			// tanggal_donor:
-			// 	traDonor.tgl_donor >= new Date(new Date().setHours(0o0, 0o0, 0o0)) &&
-			// 	traDonor.tgl_donor < new Date(new Date().setHours(23, 59, 59))
-			// 		? traDonor.tgl_donor.toISOString().split('T')[0]
-			// 		: null,
 			tanggal_donor: traDonor.tgl_donor
 				? traDonor.tgl_donor.toISOString().split('T')[0]
 				: null,
@@ -100,82 +63,11 @@ exports.getDashboardUser = async (req, res) => {
 			success: true,
 			message: 'success',
 			welcome: `Selamat datang, ${user.nama}!`,
-			// sukarelawan_menerima: userAcceptData.map(data => ({
-			// 	id_user_volunteer: data.id_user_volunteer,
-			// 	nama_volunteer: data.User.nama,
-			// 	email_volunteer: data.User.email,
-			// 	status: 'Diterima',
-			// 	alamat_volunteer: data.User.alamat,
-			// 	gol_darah: data.GolDarah.gol_darah,
-			// 	no_hp: data.User.no_hp,
-			// })),
-			// sukarelawan_menolak: userRejectData.map(data => ({
-			// 	id_user_volunteer: data.id_user_volunteer,
-			// 	nama_volunteer: data.User.nama,
-			// 	email_volunteer: data.User.email,
-			// 	status: 'Ditolak',
-			// 	alamat_volunteer: data.User.alamat,
-			// 	gol_darah: data.GolDarah.gol_darah,
-			// 	no_hp: data.User.no_hp,
-			// })),
-			// pemohon: userRequesterData.map(data => ({
-			// 	id_user: data.id_user_req,
-			// 	nama_pemohon: data.User.nama,
-			// 	email_pemohon: data.User.email,
-			// 	gol_darah: data.GolDarah.gol_darah,
-			// 	alamat_pemohon: data.User.alamat,
-			// })),
+
 			pendonor: pendonors,
 		};
 
 		res.json([response]);
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ error: 'Internal Server Error' });
-	}
-};
-
-// Controller function to accept a request
-// Controller function to accept a request (changed method to POST)
-exports.postAcceptRequest = async (req, res) => {
-	try {
-		const { id_tra_req_darah } = req.body;
-
-		// Update the status to 2 (accepted)
-		const updatedRecord = await TraReqDarah.update(
-			{ status: 2 },
-			{ where: { id_tra_req_darah } }
-		);
-
-		if (updatedRecord[0] === 0) {
-			// No records were updated (id_tra_req_darah not found)
-			return res.status(404).json({ message: 'Record not found' });
-		}
-
-		res.json({ message: 'Request accepted successfully' });
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ error: 'Internal Server Error' });
-	}
-};
-
-// Controller function to reject a request (changed method to POST)
-exports.postRejectRequest = async (req, res) => {
-	try {
-		const { id_tra_req_darah } = req.body;
-
-		// Update the status to 0 (rejected)
-		const updatedRecord = await TraReqDarah.update(
-			{ status: 0 },
-			{ where: { id_tra_req_darah } }
-		);
-
-		if (updatedRecord[0] === 0) {
-			// No records were updated (id_tra_req_darah not found)
-			return res.status(404).json({ message: 'Record not found' });
-		}
-
-		res.json({ message: 'Request rejected successfully' });
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: 'Internal Server Error' });
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,20 +1,10 @@
 // jadwal.js
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const dashboardController = require("../controllers/dashboardController");
-const { authenticateToken } = require("../middleware/middleware");
+const dashboardController = require('../controllers/dashboardController');
+const { authenticateToken } = require('../middleware/middleware');
 
 // Route to get all jadwal data
-router.get("/", authenticateToken, dashboardController.getDashboardUser);
-router.post(
-  "/acceptRequest",
-  authenticateToken,
-  dashboardController.postAcceptRequest
-);
-router.post(
-  "/rejectRequest",
-  authenticateToken,
-  dashboardController.postRejectRequest
-);
+router.get('/', authenticateToken, dashboardController.getDashboardUser);
 
 module.exports = router;
